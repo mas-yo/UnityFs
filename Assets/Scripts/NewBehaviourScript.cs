6250648a12dc7d5321eb8952b7b239b125565df6
Masahiro Yoshida <myoshida@aiming-inc.com>
2023-12-09 14:43:27 +0900

    first commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFsLib;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Say.hello("abc");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityFsLib/UnityFsLib/Library.fs
@@ -0,0 +1,6 @@
+﻿namespace UnityFsLib
+
+module Say =
+    let hello name =
+        let namePlus = name + "world"
+        printfn "Hello %s" namePlus
