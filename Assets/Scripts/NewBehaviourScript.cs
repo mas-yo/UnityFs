392f54c5f2359a158a7d5f27e17f29e49391f4aa
Masahiro Yoshida <myoshida@aiming-inc.com>
2023-12-10 21:28:09 +0900

    circle moving

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityFsLib;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Transform _transform;
+
+    private float _currentRadian = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
-        Say.hello("abc");
+
+        _transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _currentRadian = Move.updateRadian(Time.deltaTime, _currentRadian);
+        _transform.position = Move.move(_transform.position, _currentRadian);
     }
 }
--- UnityFsLib/UnityFsLib/Library.fs
+++ UnityFsLib/UnityFsLib/Library.fs
@@ -1,6 +1,26 @@
 ﻿namespace UnityFsLib
 
+open System;
+open UnityEngine
+
 module Say =
     let hello name =
         let namePlus = name + "world"
         printfn "Hello %s" namePlus
+        
+        
+        
+module Move =
+    let updateRadian (deltaTime: float32, currentRadian: float32) =
+        currentRadian + deltaTime
+        
+    let getX (radian: float32, radius: float32) =
+        float32 (float radius * Math.Sin(float radian))
+
+    let getY (radian: float32, radius: float32) =
+        float32 (float radius * Math.Cos(float radian))
+        
+    let move (position:Vector3, radian:float32) =
+        let x = getX(radian, 5.0f);
+        let y = getY(radian, 5.0f);
+        Vector3(x, y, position.z)
