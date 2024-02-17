78b735655e9d30d00b652fc76090991a9cdde645
Masahiro Yoshida <myoshida@aiming-inc.com>
2024-02-17 23:37:16 +0900

    remove comments and some stuff

--- UnityFsLib/UnityFsLib/EntityComponent.fs
+++ UnityFsLib/UnityFsLib/EntityComponent.fs
@@ -7,43 +7,8 @@ type EntityId = EntityId of int
 [<Struct>]
 type Component<'T> = { EntityId: EntityId; Value: 'T }
 
-// [<Struct>]
-// type Component2<'T1, 'T2> =
-//     { EntityId: EntityId
-//       Value1: 'T1
-//       Value2: 'T2 }
-//
-// [<Struct>]
-// type Component3<'T1, 'T2, 'T3> =
-//     { EntityId: EntityId
-//       Value1: 'T1
-//       Value2: 'T2
-//       Value3: 'T3 }
-//
-// [<Struct>]
-// type Component4<'T1, 'T2, 'T3, 'T4> =
-//     { EntityId: EntityId
-//       Value1: 'T1
-//       Value2: 'T2
-//       Value3: 'T3
-//       Value4: 'T4 }
-
 let sameEntitySelector (t2:Component<'T2> seq) (t1:Component<'T1>) =
     t2 |> Seq.find(fun x -> x.EntityId = t1.EntityId)
-
-// let withEntitySelector2old<'F, 'S>
-//     (secondSelector: Component<'F> -> Component<'S> seq -> Component<'S>)
-//     (second: Component<'S> seq)
-//     (first: Component<'F> seq)
-//     =
-//
-//     first
-//     |> Seq.map (fun f ->
-//         let s = secondSelector f second
-//
-//         { EntityId = f.EntityId
-//           Value1 = f.Value
-//           Value2 = s.Value })
     
 let withEntitySelector2<'T1, 'T2>
     (selector2: Component<'T1> -> Component<'T2>)
@@ -103,57 +68,8 @@ let withSameEntity4<'T1, 'T2, 'T3, 'T4>
     components1
     |> withEntitySelector4 (sameEntitySelector components2) (sameEntitySelector components3) (sameEntitySelector components4)
 
-// let withEntitySelector3old<'F, 'S, 'T>
-//     (thirdSelector: Component<'F> -> Component<'T> seq -> Component<'T>)
-//     (third: Component<'T> seq)
-//     (secondSelector: Component<'F> -> Component<'S> seq -> Component<'S>)
-//     (second: Component<'S> seq)
-//     (first: Component<'F> seq)
-//     =
-//
-//     first
-//     |> Seq.map (fun f ->
-//         let s = secondSelector f second
-//         let t = thirdSelector f third
-//
-//         { EntityId = f.EntityId
-//           Value1 = f.Value
-//           Value2 = s.Value
-//           Value3 = t.Value })
-//
-// let withEntitySelector4old<'T1, 'T2, 'T3, 'T4>
-//     (fourthSelector: Component<'T1> -> Component<'T4> seq -> Component<'T4>)
-//     (fourth: Component<'T4> seq)
-//     (thirdSelector: Component<'T1> -> Component<'T3> seq -> Component<'T3>)
-//     (third: Component<'T3> seq)
-//     (secondSelector: Component<'T1> -> Component<'T2> seq -> Component<'T2>)
-//     (second: Component<'T2> seq)
-//     (first: Component<'T1> seq)
-//     =
-//
-//     first
-//     |> Seq.map (fun t1 ->
-//         let t2  = secondSelector t1 second
-//         let t3 = thirdSelector t1 third
-//         let t4 = fourthSelector t1 fourth
-//
-//         { EntityId = t1.EntityId
-//           Value1 = t1.Value
-//           Value2 = t2.Value
-//           Value3 = t3.Value
-//           Value4 = t4.Value })
-
 let findByEntityId (c: Component<_>) (components: Component<_> seq) =
     components |> Seq.find (fun x -> x.EntityId = c.EntityId)
-
-// let withSameEntity2old<'F, 'S> (second: Component<'S> seq) (first: Component<'F> seq) =
-//     first |> withEntitySelector2old findByEntityId second
-//
-// let withSameEntity3old<'F, 'S, 'T> (third: Component<'T> seq) (second: Component<'S> seq) (first: Component<'F> seq) =
-//     first |> withEntitySelector3old findByEntityId third findByEntityId second
-//
-// let withSameEntity4old<'T1, 'T2, 'T3, 'T4> (fourth: Component<'T4> seq) (third: Component<'T3> seq) (second: Component<'T2> seq) (first: Component<'T1> seq) =
-//     first |> withEntitySelector4old findByEntityId fourth findByEntityId third findByEntityId second
 
 let nextValueWithSameEntity2<'T1, 'T2>
     (calcNext: 'T1 -> 'T2 -> 'T1)
--- UnityFsLib/UnityFsLib/GameLogic.fs
+++ UnityFsLib/UnityFsLib/GameLogic.fs
@@ -38,11 +38,6 @@ module GameLogic =
         |> List.map (fun x -> (GridPosition(x), 0))
         |> Map.ofList
         
-        // ||> Seq.allPairs
-        // |> Seq.map (fun x -> (GridPosition(x), 0))
-        // |> Array.ofSeq
-        // |> Map.ofArray
-
     let NewWorld =
         { GridSize = 1f
           Quickness = List.Empty
@@ -56,12 +51,12 @@ module GameLogic =
               { EntityId = EntityId(0); Value = Grid(NewGrids) }
           ] }
         
-    let toGridPosition (Position position) gridSize =
+    let toGridPosition gridSize (Position position) =
         let gx = position.X / gridSize
         let gy = position.Y / gridSize
         GridPosition(int gx, int gy)
 
-    let fromGridPosition (GridPosition(gx, gy)) gridSize =
+    let fromGridPosition gridSize (GridPosition(gx, gy)) =
         let x = gridSize * float32 gx
         let y = gridSize * float32 gy
         Vector2(x, y) |> Position        
@@ -70,7 +65,7 @@ module GameLogic =
             GridSize = world.GridSize
             Quickness = { EntityId = entityId; Value = Quickness(quickness) } :: world.Quickness 
             WaitingTimes = { EntityId = entityId; Value = WaitingTime(quickness) } :: world.WaitingTimes
-            TargetPositions = { EntityId = entityId; Value = (toGridPosition position world.GridSize) } :: world.TargetPositions
+            TargetPositions = { EntityId = entityId; Value = (toGridPosition world.GridSize position) } :: world.TargetPositions
             Directions = { EntityId = entityId; Value = Radian(0f) } :: world.Directions
             Speeds = { EntityId = entityId; Value = speed } :: world.Speeds
             Velocities = { EntityId = entityId; Value = Velocity(Vector2(0f, 0f)) } :: world.Velocities
@@ -78,46 +73,19 @@ module GameLogic =
             Grids = world.Grids
         }
 
-    let addTuple a b =
-        let a1, a2 = a
-        let b1, b2 = b
-        a1 + b1, a2 + b2
-
     let lengthSquared (Position p1) (Position p2) =
         (p1 - p2).LengthSquared()
 
-
-
-    
     let calcWaitingTime (WaitingTime waitingTime) (Quickness quickness) =
-        // let isInSameGrid gridSize pos gridPos  =
-            // gridPos = toGridPosition pos gridSize
-
         match waitingTime with
         | w when w = 0 -> quickness
         | w -> max 0 (w - 1)
-            
         |> WaitingTime
         
-    let calcTarget gridSize targetGridPos position (WaitingTime waitingTime) (Grid grid)  =
+    let calcTarget gridSize targetGridPos position (Grid grid)  =
         
-        // let searchGrid gridSize pos grid =
-            
-            // let (GridPosition(gx, gy)) = toGridPosition pos gridSize
-
-            // let dxyz =
-            //     [| (-1, 0); (1, 0); (0, -1); (0, 1); (-1, -1); (-1, 1); (1, -1); (1, 1) |]
-
-            // let targetGridPos, _ =
-            //     dxyz
-            //         |> Seq.map (fun (dx, dy) ->
-            //             let gridPos = GridPosition(gx + dx, gy + dy)
-            //             gridPos, grid.GetValueOrDefault(gridPos, 0))
-            //         |> Seq.minBy (fun (_, c) -> c)                    
-            // targetGridPos
-
         let selfGridPosition =
-            toGridPosition position gridSize
+            toGridPosition gridSize position
             
         let inSameGrid =
             targetGridPos = selfGridPosition
@@ -132,26 +100,14 @@ module GameLogic =
             let t =
                 vacants
                 |> Seq.minBy (fun x ->
-                    let gp = fromGridPosition x.Key gridSize
+                    let gp = fromGridPosition gridSize x.Key
                     lengthSquared gp position)
             t.Key
         else
             targetGridPos
                     
-        // let currentGrid = toGridPosition position gridSize
-        // let nearCount = grid.GetValueOrDefault(currentGrid, 0)
-        //     
-        // if nearCount > 1 && waitingTime = 0
-        // then
-        //     searchGrid gridSize position
-        // else
-        //     targetGridPos
-                
-        
-        
-
-    let calcDirection gridSize (direction:Radian) (Position current) (targetGridPos: GridPosition) =
-        let (Position p) = fromGridPosition targetGridPos gridSize
+    let calcDirection gridSize (_:Radian) (Position current) (targetGridPos: GridPosition) =
+        let (Position p) = fromGridPosition gridSize targetGridPos
         let diff = p - current
         atan2 diff.Y diff.X |> Radian
 
@@ -160,9 +116,9 @@ module GameLogic =
         |> Velocity
 
     let calcPosition gridSize (Position current) (Velocity velocity) (targetGridPos: GridPosition) =
-        let tp = toGridPosition (Position current) gridSize
+        let tp = toGridPosition gridSize (Position current)
         
-        let (Position p) = fromGridPosition targetGridPos gridSize
+        let (Position p) = fromGridPosition gridSize targetGridPos
         let l1 = (current - p).LengthSquared()
         let l2 = (current + velocity - p).LengthSquared()
         
@@ -178,7 +134,7 @@ module GameLogic =
 
         let countMap =
             positions
-            |> Seq.groupBy (fun p -> toGridPosition p gridSize)
+            |> Seq.groupBy (fun p -> toGridPosition gridSize p)
             |> Seq.map (fun (k, values) -> k, Seq.length values)
             |> Map.ofSeq
         
@@ -198,14 +154,13 @@ module GameLogic =
 
         let nextTargets =
             world.TargetPositions
-            |> withEntitySelector4
+            |> withEntitySelector3
                    (sameEntitySelector world.CurrentPositions)
-                   (sameEntitySelector nextWaitingTimes)
                    (gridSelector world.Grids)
-            |> Seq.map (fun (entityId, target, position, waitingTime, grid) ->
+            |> Seq.map (fun (entityId, target, position, grid) ->
                                     {
                                         EntityId = entityId
-                                        Value = calcTarget world.GridSize target position waitingTime grid
+                                        Value = calcTarget world.GridSize target position grid
                                     })
             |> List.ofSeq
 
@@ -226,13 +181,6 @@ module GameLogic =
 
         let nextGridInfo =
             calcGrid world.GridSize world.Grids.Head.Value (nextPosition |> Seq.map (fun p -> p.Value))
-            // nextPosition
-            // |> Seq.map (fun p -> p.Value)
-            // |> (calcGrid world.GridSize)
-            
-            // |> withEntitySelector2 (gridSelector world.Grids)
-            // |> Seq.map (fun (entityId, value1, value2) -> calcGrid world.GridSize value1 value2)
-            // |> Grid
 
         let nextGrid =
             [ { EntityId = EntityId(0)
