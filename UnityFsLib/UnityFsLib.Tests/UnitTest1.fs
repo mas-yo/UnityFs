ac2e4127a45416b4d1303da7ef9e2b78b1805a2b
Masahiro Yoshida <myoshida@aiming-inc.com>
2024-02-17 23:24:51 +0900

    test calcWaitingTime

--- UnityFsLib/UnityFsLib.Tests/UnitTest1.fs
+++ UnityFsLib/UnityFsLib.Tests/UnitTest1.fs
@@ -1,11 +1,17 @@
 module UnityFsLib.Tests
 
 open NUnit.Framework
+open UnityFsLib.GameLogic
 
 [<SetUp>]
 let Setup () =
     ()
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let TestCalcWaitingTime () =
+    let (WaitingTime result) = calcWaitingTime (WaitingTime 10) (Quickness 10)
+    Assert.AreEqual(9, result)
+    
+    let (WaitingTime result) = calcWaitingTime (WaitingTime 0) (Quickness 10)
+    Assert.AreEqual(10, result)
+
